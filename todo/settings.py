76c0689ede0fb3ce90dd01cf19bad0fb2aee1c6a
Unique Maharjan <unique.maharzn@gmail.com>
2022-01-08 14:35:24 +0545

    fifth commit

--- todo/settings.py
+++ todo/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-MEDIA_URL = '/images/'
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [BASE_DIR/'static',]
 
